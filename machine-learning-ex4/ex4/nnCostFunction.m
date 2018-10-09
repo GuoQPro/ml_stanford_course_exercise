8f7fd7445b27080d55d58c874b1478d208e01773
MrSep18th <mrsep18th@gmail.com>
2018-10-09 17:18:44 +0800

    part1

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -64,20 +64,23 @@ Theta2_grad = zeros(size(Theta2));
 
 
 
-x_temp = [ones(m, 1) X]; % 5000x401
+x_temp = [ones(m, 1) X]; % add bias to input, and the result is 5000x401
 a2 = sigmoid(Theta1 * x_temp'); % 25x5000
-a2_temp = [ones(1, m); a2]; % 26x5000 
+a2_temp = [ones(1, m); a2]; % add bias to hiden layer, result is 26x5000 
 a3 = sigmoid(Theta2 * a2_temp); % 10x5000
-h = a3;
-y_temp = zeros(num_labels, m); 
+h_theta = a3;
+y_temp = zeros(num_labels, m); % 10x5000
 
 for i = 1:m
     value = y(i);
     y_temp(value, i) = 1;
 end
 
-% note the .*
-J = sum(sum(-1 * y_temp .* log(h) - (1 - y_temp) .* log(1 - h), 2)) / m;
+% note the symbol .*
+% my own understanding: for the multi-class classification problem, only the value in h_theta corresponding to 
+% the predicted class should be considered meaningful.
+
+J = sum(sum(-y_temp .* log(h_theta) - (1 - y_temp) .* log(1 - h_theta))) / m;
 
 
 
